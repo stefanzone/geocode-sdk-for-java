a67c2f06f333da52f306b172f311d75e5a80db2e
Stefan Kühnel <git@stefankuehnel.com>
2021-01-07 16:10:34 +0100

    Fix Bug #2.

--- src/main/java/zone/stefan/dev/geocode/http/Fetch.java
+++ src/main/java/zone/stefan/dev/geocode/http/Fetch.java
@@ -1,10 +1,12 @@
 package zone.stefan.dev.geocode.http;
 
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
+import zone.stefan.dev.geocode.exception.HttpStatusCodeException;
 
-/*
+/**
  * Fetch.java
  * @author Stefan Kühnel, stefan.kuehnel@hm.edu
  * @version 2020-12-31
@@ -12,14 +14,11 @@ import java.util.Scanner;
 
 public class Fetch {
     /**
-     * Ruft den Inhalt einer Remote Ressource ab.
-     * @param resource URL zur Remote Ressource.
-     * @return Den abgerufen Inhalt der Remote Ressource.
+     * Ruft den Inhalt einer entfernten Ressource ab.
+     * @param resource URL zur entfernten Ressource.
+     * @return Der abgerufene Inhalt der entfernten Ressource.
      */
     public static String fetch(String resource) {
-        // Der Inhalt der abgefragten Ressource.
-        StringBuilder inline = new StringBuilder();
-
         try {
             URL url = new URL(resource);
 
@@ -32,24 +31,36 @@ public class Fetch {
 
             // Bei der Bearbeitung der Anfrage ist ein Fehler aufgetreten.
             if (responseCode != 200) {
-                throw new RuntimeException("HttpResponseCode: " + responseCode);
+                throw new HttpStatusCodeException("HttpResponseCode: " + responseCode);
             }
-
-            Scanner scanner = new Scanner(url.openStream());
-
-            // Nutzung eines Scanners zum Schreiben der erhaltenen Daten.
-            while (scanner.hasNext()) {
-                inline.append(scanner.nextLine());
-            }
-
-            // Schließen des Scanners.
-            scanner.close();
 
             // Ausgabe der erhaltenen Daten.
-            return inline.toString();
-        } catch (Exception e) {
+            return getContent(url.openStream());
+        } catch (Exception exception) {
             // Es ist ein Fehler aufgetreten.
             return null;
         }
+    }
+
+    /**
+     * Schreibt den Inhalt der entfernten Ressource in eine Zeichenkette.
+     * @param inputStream Ein InputStream, der das Lesen von der entfernten Ressource ermöglicht.
+     * @return Der Inhalt der entfernten Ressource als Text.
+     */
+    private static String getContent(InputStream inputStream) {
+        // Enthält den Inhalt der entfernten Ressource.
+        final StringBuilder content = new StringBuilder();
+
+        try (Scanner scanner = new Scanner(inputStream)) {
+
+            // Liest den Inhalt des InputStreams Zeile für Zeile in den StringBuilder ein.
+            while (scanner.hasNext()) {
+                content.append(scanner.nextLine());
+            }
+
+        }
+
+        // Rückgabe der erhaltenen Daten.
+        return content.toString();
     }
 }
